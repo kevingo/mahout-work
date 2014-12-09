f527188b6785858e98865b691d7a480be3822279
kevingo <kevingo75@gmail.com>
2014-12-10 00:32:37 +0800

    Finish basic ItemBasedRecommend

--- recommender/src/main/java/recommender/ItemBasedRecommend.java
+++ recommender/src/main/java/recommender/ItemBasedRecommend.java
@@ -2,19 +2,36 @@ package recommender;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class ItemBasedRecommend {
 	
 	public static void main(String [] args) {
 		try {
-			DataModel model = new FileDataModel(new File(""));
-			
+			DataModel model = new FileDataModel(new File("ua.base"));
+			ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
+			Recommender recommend = new GenericItemBasedRecommender(model, similarity);
+			List<RecommendedItem> recommendations = recommend.recommend(1, 5);
+			System.out.println(recommendations);
 			
 			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TasteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
